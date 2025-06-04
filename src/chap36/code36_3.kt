0d0d02b581c27f085268e33f0685ef803497da06
itc-s24001 <s24001@std.it-college.ac.jp>
2025-06-04 13:58:25 +0900

    36章まで

---
+++ src/chap36/code36_3.kt
@@ -0,0 +1,21 @@
+package chap36
+
+fun main() {
+    val p = Person()
+    p.setNameAndAge("タケシ", 5)
+    p.greet()
+}
+
+class Person {
+    var name = ""
+    var age = 0
+
+    fun setNameAndAge(name: String, age: Int) {
+        this.name = name
+        this.age = age
+    }
+
+    fun greet() {
+        println("${name}です。${age}才です。")
+    }
+}
